6da1ef6acd5d99904430b25a9b7232c7661788a7
safo samson <safosamson18@gmail.com>
2024-02-29 22:29:02 +0000

    added web configuration to it

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -17,10 +17,7 @@ import 'package:flutter/foundation.dart'
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web - '
-        'you can reconfigure this by running the FlutterFire CLI again.',
-      );
+      return web;
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
@@ -28,10 +25,7 @@ class DefaultFirebaseOptions {
       case TargetPlatform.iOS:
         return ios;
       case TargetPlatform.macOS:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for macos - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
+        return macos;
       case TargetPlatform.windows:
         throw UnsupportedError(
           'DefaultFirebaseOptions have not been configured for windows - '
@@ -49,6 +43,15 @@ class DefaultFirebaseOptions {
     }
   }
 
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyCMxImrX4KoPsNIc7cezM4moY-CTP0fIBs',
+    appId: '1:840918420570:web:98cb828f0f5e2b912b9c63',
+    messagingSenderId: '840918420570',
+    projectId: 'lsbu-uni-connect',
+    authDomain: 'lsbu-uni-connect.firebaseapp.com',
+    storageBucket: 'lsbu-uni-connect.appspot.com',
+  );
+
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyCSaCQMfYU-NjSfm8p_9uVjSM7B_4LD1tE',
     appId: '1:840918420570:android:8004fdb0a4333a892b9c63',
@@ -64,5 +67,14 @@ class DefaultFirebaseOptions {
     projectId: 'lsbu-uni-connect',
     storageBucket: 'lsbu-uni-connect.appspot.com',
     iosBundleId: 'com.kanta.uniconnect',
+  );
+
+  static const FirebaseOptions macos = FirebaseOptions(
+    apiKey: 'AIzaSyCrODojJn8h6-0I9PwPRpsAN0m7bOzO05A',
+    appId: '1:840918420570:ios:5f712e12392c424c2b9c63',
+    messagingSenderId: '840918420570',
+    projectId: 'lsbu-uni-connect',
+    storageBucket: 'lsbu-uni-connect.appspot.com',
+    iosBundleId: 'com.kanta.uniconnect.RunnerTests',
   );
 }
