9292339a72469b2b2b68700a6b6e70244d0b175b
safo samson <safosamson18@gmail.com>
2024-03-04 22:07:58 +0000

    tweaks to add emojies to the ice breakers

--- lib/constants/ice_breaker_messages.dart
+++ lib/constants/ice_breaker_messages.dart
@@ -2,6 +2,10 @@ import 'dart:math';
 
 import 'package:uniconnect/widgets/user_profile.dart';
 
+
+
+
+
 class IceBreakerGenerator {
   static String generateIceBreakerMessage(
       UserProfile currentUser, UserProfile otherUser) {
@@ -29,7 +33,7 @@ class IceBreakerGenerator {
     if (commonProfiles.isEmpty) {
       // If no common profiles found, generate a generic message
       iceBreakerMessage +=
-          "I noticed we have different backgrounds, but I feel we'd make great friends. Instincts 😂 you know? How do you find LSBU and ${otherUser.course} ?";
+          "I noticed we have different backgrounds, but I feel we'd make great friends. Instincts 😂 you know? '${moreInterestingTopics[Random().nextInt(moreInterestingTopics.length)]}'";
     } else {
       // If common profiles found, generate a personalized message
       iceBreakerMessage += "I noticed we have ";
@@ -45,21 +49,30 @@ class IceBreakerGenerator {
 
       // Add a random interesting question or topic
       List<String> interestingTopics = [
-        '\nWhat do you enjoy most about your course?',
-        '\nHave you traveled to any interesting places recently?',
-        '\nDo you have any favorite hobbies or activities?',
-        '\nWhat are your career aspirations after graduation?',
-        '\nDo you have any favorite books or movies?',
-        '\nWhat is the most exciting thing you have ever done?',
-        '\nWhere you born in ${otherUser.country} or just came to the UK for University?',
-        '\nWhat is your favorite thing about ${otherUser.course}?',
-        "\nHow's life in ${otherUser.year}?",
+        '\nWhat do you enjoy most about your course? 📚',
+        '\nHave you traveled to any interesting places recently? ✈️',
+        '\nDo you have any favorite hobbies or activities? 🎨',
+        '\nWhat are your career aspirations after graduation? 💼',
+        '\nDo you have any favorite books or movies? 📖🎥',
+        '\nWhat is the most exciting thing you have ever done? 🚀',
+        '\nWhere you born in ${otherUser.country} or just came to the UK for University? 🌍',
+        '\nWhat is your favorite thing about ${otherUser.course}? 🤔',
+        "\nHow's life in ${otherUser.year}? 🎓",
       ];
 
       iceBreakerMessage +=
-          '${interestingTopics[Random().nextInt(interestingTopics.length)]}?';
+          interestingTopics[Random().nextInt(interestingTopics.length)];
     }
 
     return iceBreakerMessage;
   }
 }
+
+List<String> moreInterestingTopics = [
+  '\nWhat do you enjoy most about your course? 📚',
+  '\nHave you traveled to any interesting places recently? ✈️',
+  '\nDo you have any favorite hobbies or activities? 🎨',
+  '\nWhat are your career aspirations after graduation? 💼',
+  '\nDo you have any favorite books or movies? 📖🎥',
+  '\nWhat is the most exciting thing you have ever done? 🚀',
+];
